1652c497292fe2717f566e8bcf406b474897ff08
AbdulmalikAlayande <114596864+AbdulmalikAlayande@users.noreply.github.com>
2025-09-13 17:06:39 +0100

    Add corsheaders to installed apps and middleware

--- amala_atlas/settings.py
+++ amala_atlas/settings.py
@@ -34,9 +34,11 @@ INSTALLED_APPS = [
     'django_extensions.apps.DjangoExtensionsConfig',
     'django_filters',
     'rest_framework.apps.RestFrameworkConfig',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
